9fd2a39102ae1d8b33af26bc9e727a2689468da2
benurgh <benurgh@gmail.com>
2020-12-22 18:13:22 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,19 +1,15 @@
-def arr_sort(arr)
-  not_exit = true
-  max_index = arr.length - 2
-
-  while not_exit
-    not_exit = false
-    (0..max_index).each do |i|
-      element = arr[i]
-      next_element = arr[i+1]
-      if element > next_element
-        arr[i], arr[i+1]= next_element, element
-        not_exit = true
-      end
-    end
-  end
-  arr
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
 end
 
-p arr_sort([1,54,6345,22,4])
+say_hello_world_ten_times
